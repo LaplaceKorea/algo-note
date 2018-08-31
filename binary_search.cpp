d62ac374e536b2e25a80e7a72ac2f918366f729b
ryanking13 <def6488@gmail.com>
2018-08-31 16:48:41 +0900

    update binary search example

--- binary_search.cpp
+++ binary_search.cpp
@@ -5,8 +5,9 @@ using namespace std;
 
 int main(){
 
-    vector<int> v(10);
+    vector<int> v;
     for(int i = 0; i < 10; i++) v.push_back(i);
+    for(int i = 11; i < 21; i++) v.push_back(i);
 
     int lower_idx = lower_bound(v.begin(), v.end(), 5) - v.begin();
     int upper_idx = upper_bound(v.begin(), v.end(), 5) - v.begin();
@@ -17,4 +18,19 @@ int main(){
     
     printf("lower: %d\n", v[lower_idx]);
     printf("upper: %d\n", v[upper_idx]);
+
+
+    int start = 0;
+    int end = 20 - 1;
+
+    while (start <= end) {
+        int mid = (start + end) / 2;
+        if (v[mid] < 10) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+
+    printf("bsearch: %d\n", v[start]);
 }
